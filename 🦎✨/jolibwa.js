7357f357d21725538d7b2a1a35016e5c57600996
Jay123437 <woodisme19@gmail.com>
2026-09-06 11:10:20 -0300

    Delete 🦎✨ directory

--- 🦎✨/jolibwa.js
+++
@@ -1,5 +0,0 @@
-// Pont de compatibilité : les plugins font require('../jolibwa'),
-// mais le module réel (cmd, commands, AddCommand, Function) vit dans inconnuboy.js.
-// On réexporte exactement le même objet/tableau pour que toutes les commandes
-// s'enregistrent dans le même registre partagé que celui utilisé par inconnu.js.
-module.exports = require('./inconnuboy');
