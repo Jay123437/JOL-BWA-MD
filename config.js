006224954e87664b73863e2acc8aa901cd98c5e7
PRINCE-PREMIUM-MDX <rulxmullerjeangilles@gmail.com>
2026-09-14 00:19:33 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ module.exports = {
     // 2. INFORMATIONS DU BOT
     // ===========================================================
     PREFIX: process.env.PREFIX || '.',
-    OWNER_NUMBER: process.env.OWNER_NUMBER || '50940616026', // Mettez votre numéro ici
+    OWNER_NUMBER: process.env.OWNER_NUMBER || '18296955078', // Mettez votre numéro ici
     OWNER_NAME: process.env.OWNER_NAME || "Jol!bwa 🏌🏻‍♂️ 🤍",
     BOT_NAME: "JOL!BWA-MD",
     BOT_FOOTER: '© ᴘᴏᴡᴇʀᴇᴅ ʙʏ jolibwa Bel bwa🏌🏻‍♂️',
